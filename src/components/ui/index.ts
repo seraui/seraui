9b62f1244cd1477e67ed91fb619af6258b34261d
seraprogrammer <programmernazmul1@gmail.com>
2025-07-03 16:05:36 +0600

    Feature update

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,5 +2,4 @@ export { LoadingBar } from "./loading-bar";
 export { LoadingProvider, useLoading } from "./loading-provider";
 export { LoadingLink } from "./loading-link";
 export { useSmartLoading, useComponentLoading, useRouteLoading } from "./use-smart-loading";
-export { LoadingDemo } from "./loading-demo";
 export { BorderBeam } from "./border-beam";
